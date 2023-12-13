f4be254d494f434a739ce01992354b5fe1dae037
Nitramcr <martingolramirez@gmail.com>
2023-12-13 19:17:56 -0300

    Proyecto casi finalizado. Falta agregar contenido a la seccion de projects y agregar el idioma español a la web

---
+++ src/data/info.js
@@ -0,0 +1,69 @@
+export const info = {
+  baseUrl: 'https://nitramcr',
+  name: 'Martín Ramírez',
+  jobDescription: 'Frontend Developer',
+  about: [
+    'I’m a Jr. Frontend Developer based in Mendoza, Argentina. I’m already looking to find my first experience as a Web Developer.',
+    'More of 5 years competing at a high level in esports have given me skills such as teamwork, communication, problem solving and adaptability under pressure.',
+    'My technological stack is based on HTML, CSS, JavaScript, React and Tailwind. I also gained some knowledge of Astro, NodeJS and TypeScript.',
+    'As a autodidact, I learn continuously and never stop exploring new technologies.',
+  ],
+
+  experience: [
+    {
+      name: 'TeamQueso',
+      location: 'Spain, Madrid',
+      startDate: '2017 /',
+      endDate: '2018',
+      description: [
+        '- Main achievements: 1st place in ESWC Latam, and 15th in ESWC Paris Games Week.',
+      ],
+    },
+
+    {
+      name: 'Sandstorm',
+      location: 'México, CDMX',
+      startDate: '2018',
+      description: [
+        '- Main achievements: 6th place in CRL Latam, competition that lasted 3 months in CDMX.',
+      ],
+    },
+
+    {
+      name: 'Tribe Gaming',
+      location: 'United States, Texas',
+      startDate: '2019 /',
+      endDate: '2020',
+      description: [
+        '- I competed for 6 months in the CRL West in Los Angeles.',
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: 'Autodidact studies',
+      description: [
+        '- +30 courses completed on platforms such as Platzi and edX - Certificates: ',
+        '- Independent learning capacity, fast and effective. I’m always learning new technologies.',
+      ],
+    },
+  ],
+
+  socialMedia: {
+    twitter: 'https://twitter.com/NitramCR',
+    github: 'https://github.com/nitramcr',
+    email: 'mailto:nitramcr0@gmail.com',
+    linkedin: 'https://www.linkedin.com/in/nitramcr/',
+  },
+
+  projects: [
+    {
+      title: 'Web using React & framer-motion',
+      isFeatured: true,
+      thumbnail: '/assets/images/web-framer-motion.png',
+      githubUrl: 'https://github.com/nitramcr/',
+      liveUrl: 'https://github.com/nitramcr/',
+    },
+  ],
+}
